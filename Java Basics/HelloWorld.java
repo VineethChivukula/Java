55a568df18489169baf25aeddae34060934db509
vineethchivukula <qualityhacker2002@gmail.com>
2023-08-26 16:45:32 +0530

    commit

---
+++ Java Basics/HelloWorld.java
@@ -0,0 +1,21 @@
+// This is a single-line comment in Java. It is used to provide explanations and documentation.
+
+public class HelloWorld {
+    // This is the main entry point of our Java program.
+    public static void main(String[] args) {
+        // 'public' is an access modifier. It means that the main method can be accessed from outside the class.
+        // 'static' means that the method belongs to the class itself, not to any particular instance of the class.
+        // 'void' indicates that the main method does not return any value.
+        // 'main' is the name of the method, and it's the entry point for our program.
+        // 'String[] args' is the parameter list. It allows us to pass command-line arguments to our program.
+
+        // This line of code prints "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+
+        // 'System' is a built-in class in Java that provides access to system-related resources.
+        // 'out' is a static member of the System class, representing the standard output stream.
+        // 'println' is a method of the 'out' object, used to print a string to the console followed by a newline.
+
+        // The program execution starts at the main method, and this line is the first to be executed.
+    }
+}
